3c1940c829efd24fee8c4c4d99d4a55dee03ec6c
zhamila <itmasterhaha@gmail.com>
2025-11-01 03:50:53 +0300

    feat: add TaskGamer Telegram bot project with Spring Boot

---
+++ src/main/java/com/zhamila/taskgamer_bot/TaskgamerBotApplication.java
@@ -0,0 +1,13 @@
+package com.zhamila.taskgamer_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskgamerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskgamerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhamila/taskgamer_bot/TaskgamerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhamila.taskgamer_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskgamerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
